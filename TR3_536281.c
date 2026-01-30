e814aea1ed137365f89727924b7f077461af9e66
Marcos Rodrigues <59182008+zMarcoos@users.noreply.github.com>
2026-01-29 23:46:32 -0300

    Clean up comments and format in TR3_536281.c
    Removed comments and cleaned up code formatting.

--- TR3_536281.c
+++ TR3_536281.c
@@ -2,13 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-
-  //Variáveis que podem user usadas
-  //Atribuindo com {0} ele zera todos os bits para que não haja lixo de memória
   unsigned int x[200] = {0};
   int y, i;
 
-  //Leitura dos números e marcação dos bits correspondentes no vetor
   while (1) {
     printf("Digite um número (0 a 5000): ");
     scanf("%d", &y);
@@ -18,7 +14,6 @@ int main() {
     x[y/32] = x[y/32] | (1 << (y % 32));
   }
 
-  //Imprime os números que foram marcados no vetor
   for (i = 0; i <= 5000; i++) {
     if ((x[i/32] >> i) & 1) {
       printf("%d\n", i);
